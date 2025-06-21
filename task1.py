1b0a35bd8493fc231058890fa0aefb4c902a4961
sanskriti5002 <146509881+sanskriti5002@users.noreply.github.com>
2025-06-22 01:50:49 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,11 @@
+# Problem Statement:  Write a Python program that:
+# 1. 	Takes an integer input from the user.
+# 2. 	Checks whether the number is even or odd using an if-else statement.
+# 3. 	Displays the result accordingly.
+
+
+number = int(input("Enter a number :"))
+if number % 2 == 0:
+    print("number is even")
+else:
+    print("number is odd")
